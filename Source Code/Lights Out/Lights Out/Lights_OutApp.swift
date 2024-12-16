1716c20b8963de5cfac09c32aad77efc11ddd704
Mark Howard <markhoward2005@gmail.com>
2024-12-16 19:47:53 +0000

    Uploaded Source Code

---
+++ Source Code/Lights Out/Lights Out/Lights_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lights_OutApp.swift
+//  Lights Out
+//
+//  Created by Mark Howard on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lights_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
